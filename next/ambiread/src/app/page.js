0d98414468fb2872eaa53ac825f20cd0138c8861
Michael Buholzer <michael.buholzer@outlook.com>
2024-12-07 22:02:37 +0100

    checkpoint

--- next/ambiread/src/app/page.js
+++ next/ambiread/src/app/page.js
@@ -1,6 +1,6 @@
 'use client';
 
-import Image from "next/image";
+// import Image from "next/image";
 import styles from "./page.module.css";
 
 class BackgroundMusicPlayer {
@@ -181,9 +181,9 @@ class BackgroundMusicPlayer {
 
 export default function Home() {
   const player = new BackgroundMusicPlayer();
+  let page = 0;
   
-  const generateAndPlay = async (e) => {
-    e.preventDefault();
+  const generateAndPlay = async () => {
     const response = await fetch("/backend", {
       method: "POST",
       headers: {
@@ -198,95 +198,121 @@ export default function Home() {
     await player.play(url);
   }
 
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <button onClick={generateAndPlay}>Click</button>
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const goToPreviousPage = async (e) => {
+    e.preventDefault();
+    console.log('Previous page');
+    generateAndPlay();
+  }
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+  const goToNextPage = async (e) => {
+    e.preventDefault();
+    console.log('Next page');
+  }
+
+  const text = "This is a test text";
+
+  return (
+    // <div className={styles.page}>
+    //   <main className={styles.main}>
+    //     <Image
+    //       className={styles.logo}
+    //       src="/next.svg"
+    //       alt="Next.js logo"
+    //       width={180}
+    //       height={38}
+    //       priority
+    //     />
+    //     <button onClick={generateAndPlay}>Click</button>
+    //     <ol>
+    //       <li>
+    //         Get started by editing <code>src/app/page.js</code>.
+    //       </li>
+    //       <li>Save and see your changes instantly.</li>
+    //     </ol>
+
+    //     <div className={styles.ctas}>
+    //       <a
+    //         className={styles.primary}
+    //         href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+    //         target="_blank"
+    //         rel="noopener noreferrer"
+    //       >
+    //         <Image
+    //           className={styles.logo}
+    //           src="/vercel.svg"
+    //           alt="Vercel logomark"
+    //           width={20}
+    //           height={20}
+    //         />
+    //         Deploy now
+    //       </a>
+    //       <a
+    //         href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+    //         target="_blank"
+    //         rel="noopener noreferrer"
+    //         className={styles.secondary}
+    //       >
+    //         Read our docs
+    //       </a>
+    //     </div>
+    //   </main>
+    //   <footer className={styles.footer}>
+    //     <a
+    //       href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       <Image
+    //         aria-hidden
+    //         src="/file.svg"
+    //         alt="File icon"
+    //         width={16}
+    //         height={16}
+    //       />
+    //       Learn
+    //     </a>
+    //     <a
+    //       href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       <Image
+    //         aria-hidden
+    //         src="/window.svg"
+    //         alt="Window icon"
+    //         width={16}
+    //         height={16}
+    //       />
+    //       Examples
+    //     </a>
+    //     <a
+    //       href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       <Image
+    //         aria-hidden
+    //         src="/globe.svg"
+    //         alt="Globe icon"
+    //         width={16}
+    //         height={16}
+    //       />
+    //       Go to nextjs.org →
+    //     </a>
+    //   </footer>
+    // </div>
+
+    <div>
+      <header className={styles.header}>
+        <h1>AmbiRead</h1>
+      </header>
+      <section className={styles.section}>
+        <button className={styles.previousPage} onClick={goToPreviousPage}>&lt;</button>
+        <button className={styles.nextPage} onClick={goToNextPage}>&gt;</button>
+        <div className={styles.textContainer}>
+          <p>{text}</p>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </section>
     </div>
   );
 }
